696d6cfe127fe87365804722370101244a557827
japsolo <jh.docente@gmail.com>
2019-07-30 11:35:33 -0300

    Refactor de la validación con JS

--- public/js/validateMoviesCreate.js
+++ public/js/validateMoviesCreate.js
@@ -13,6 +13,9 @@ formInputs.pop();
 // Expresión regular para validar solo números
 var regexNumber = /^\d+$/;
 
+// Objeto literal para verificar si un campo tiene error
+var errorsObj = {};
+
 // Recorremos el array y asignamos la validación básica
 formInputs.forEach(function (oneInput) {
 	// A cada campo le asignamos el evento blur y su funcionalidad
@@ -23,6 +26,8 @@ formInputs.forEach(function (oneInput) {
 			this.classList.add('is-invalid');
 			// Ademas, al <div> con clase 'invalid-feedback' le agremamos el texto de error
 			this.nextElementSibling.innerHTML = 'El campo <b>' + this.getAttribute('data-nombre') + '</b> es obligatorio';
+			// Si un campo tiene error, creamos una key con el nombre del campo y valor true
+			errorsObj[this.name] = true;
 		} else {
 			// Cuando el campo NO está vacío
 
@@ -35,12 +40,17 @@ formInputs.forEach(function (oneInput) {
 			// Al mensaje de error le sacamos el texto
 			this.nextElementSibling.innerHTML = '';
 
+			// Si un campo NO tiene error, eliminamos la key del objeto y su valor
+			delete errorsObj[this.name];
+
 			// Validamos el tipo de dato del campo title
 			if (this.name === 'title') {
 				// Validamos que el texto insertado NO supere las 15 letras
 				if (this.value.length > 15) {
 					this.classList.add('is-invalid');
 					this.nextElementSibling.innerHTML = 'El título debe ser inferior a 15 letras';
+					// Si un campo tiene error, creamos una key con el nombre del campo y valor true
+					errorsObj[this.name] = true;
 				}
 			}
 
@@ -49,30 +59,45 @@ formInputs.forEach(function (oneInput) {
 				if (!regexNumber.test(this.value.trim())) {
 					this.classList.add('is-invalid');
 					this.nextElementSibling.innerHTML = 'Este campo solo admite números';
+					// Si un campo tiene error, creamos una key con el nombre del campo y valor true
+					errorsObj[this.name] = true;
 				}
 			}
 		}
 	});
 
-	oneInput.addEventListener('focus', function () {
-		var inputName = this.name;
-		switch (inputName) {
-			case 'title':
-				console.log('Título de la película inferior a 15 letras');
-				break;
-			case 'rating':
-				console.log('Solamente números');
-				break;
-		};
-	});
+	// oneInput.addEventListener('focus', function () {
+	// 	var inputName = this.name;
+	// 	switch (inputName) {
+	// 		case 'title':
+	// 			console.log('Título de la película inferior a 15 letras');
+	// 			break;
+	// 		case 'rating':
+	// 			console.log('Solamente números');
+	// 			break;
+	// 	};
+	// });
 });
 
 // Si tratan de enviar el formulario
 theForm.addEventListener('submit', function (event) {
-	// Pregunto si el primer campo (título) ó el segundo campo (rating) están vació
-	if (formInputs[0].value.trim() === '' || formInputs[1].value.trim() === '') {
-		// Si está vacío evito que se envíe la data
+	// Al momento de SUBMITEAR el formulario iteramos los campos y validamos si están vacíos
+	formInputs.forEach(function (input) {
+		if (input.value.trim() === '') {
+			// Si el campo está vacío creamos dentro del objeto de errores una key con valor true
+			errorsObj[input.name] = true;
+			// Asiganmos la clase de CSS
+			input.classList.add('is-invalid');
+			// Mostramos el mensaje de error
+			input.nextElementSibling.innerHTML = 'El campo <b>' + input.getAttribute('data-nombre') + '</b> es obligatorio';
+		}
+	});
+
+	console.log('Campos con errores:', errorsObj);
+	console.log('Cantidad de campos con errores:', Object.keys(errorsObj).length);
+
+	// Si el objeto que contiene los errores NO está vacío evitamos que se SUBMITEE el formulario
+	if (Object.keys(errorsObj).length > 0) {
 		event.preventDefault();
-		window.alert('Hay campos vacíos');
 	}
 });
