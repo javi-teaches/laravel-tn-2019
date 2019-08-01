4ba51694cc4732c151cd097f67ee9c8da7d43c00
japsolo <jh.docente@gmail.com>
2019-08-01 16:21:05 -0300

    Refactor Fetch Calls

--- public/js/fetch.js
+++ public/js/fetch.js
@@ -3,39 +3,41 @@ window.addEventListener('load', function () {
 	var selectProvincias = document.querySelector('#city');
 	var contenedorProvincias = selectProvincias.parentElement.parentElement;
 
-	fetch('https://restcountries.eu/rest/v2/all')
-		.then(function (response) {
-			return response.json(); // array de objetos literales
-		})
-		.then(function (data) {
-			data.forEach(function (unPais) {
-				var option = document.createElement('option');
-				option.innerText = unPais.name;
-				option.setAttribute('value', unPais.name);
-				selectPaises.append(option);
-				// selectPaises.innerHTML += `<option value="${unPais.name}">${unPais.name}</option>`;
+	// Función genérica para hacer llamados FETCH
+	function genericFetchCall (endPoint, callback) {
+		fetch(endPoint)
+			.then(function (response) {
+				return response.json(); // array de objetos literales
+			})
+			.then(function (data) {
+				callback(data);
+			})
+			.catch(function (error) {
+				console.log('El error fue: ' + error);
 			});
-		})
-		.catch(function (error) {
-			console.log('El error fue: ' + error);
+	}
+
+	// Función para traer los países e insertarlos en el selectPaises
+	function insertCountries (countries) {
+		countries.forEach(function (oneCountry) {
+			selectPaises.innerHTML += `<option value="${oneCountry.name}">${oneCountry.name}</option>`;
 		});
+	}
+
+	// Llamamos a la api de Paises
+	genericFetchCall('https://restcountries.eu/rest/v2/all', insertCountries);
+
+	// Función para traer las provincias e insertarlas en el selectProvincias
+	function insertProvinces (provinces) {
+		provinces.provincias.forEach(function (unaProvincia) {
+			selectProvincias.innerHTML += `<option value="${unaProvincia.nombre}">${unaProvincia.nombre}</option>`;
+		});
+	}
 
 	selectPaises.addEventListener('change', function () {
 		if (this.value.toLowerCase() === 'argentina') {
-			contenedorProvincias.style.display = 'block';
-
-			fetch('https://apis.datos.gob.ar/georef/api/provincias')
-				.then(function (response) {
-					return response.json(); // array de objetos literales
-				})
-				.then(function (data) {
-					data.provincias.forEach(function (unaProvincia) {
-						selectProvincias.innerHTML += `<option value="${unaProvincia.nombre}">${unaProvincia.nombre}</option>`;
-					});
-				})
-				.catch(function (error) {
-					console.log('El error fue: ' + error);
-				});
+			contenedorProvincias.style.display = 'flex';
+			genericFetchCall('https://apis.datos.gob.ar/georef/api/provincias', insertProvinces);
 		} else {
 			contenedorProvincias.style.display = 'none';
 			selectProvincias.innerHTML = '<option value="">Elegí una provincia</option>';
